48e0f6d0c346a8138bd0af85534a76821120fb93
vamsee. <4n0n5ur7@protonmail.com>
2020-12-26 14:16:43 +0530

    used loop for dead cells

--- src/cgol.java
+++ src/cgol.java
@@ -8,31 +8,30 @@
  */
 
 public class cgol {
-	
+
 	private Integer edge;
 	int[][] currentGen;
 	int[][] nextGen;
 	private cgolWindow winDisplay;
-	
+
 	public cgol(Integer dimen) {
 		this.winDisplay = new cgolWindow(dimen);
 		this.edge = dimen - 1;
 		this.nextGen = new int[dimen][dimen];
 		currentGen = createRandomStart(dimen);
 	}
-	
+
 	public cgol(Integer dimen, int[][] startmatrix) {
 		this.winDisplay = new cgolWindow(dimen);
 		this.edge = dimen - 1;
 		this.nextGen = new int[dimen][dimen];
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		cgol simule = new cgol(50);
 		int[][] endingWorld = simule.simulate(50);
 	}
-	
 
 	/**
 	 * @param maxGen
@@ -40,26 +39,25 @@ public class cgol {
 	 */
 	private int[][] simulate(Integer maxGen) {
 		// TODO Auto-generated method stub
-		for(int i = 0; i <= maxGen; i++) {
+		for (int i = 0; i <= maxGen; i++) {
 			this.winDisplay.display(currentGen, i);
 			this.winDisplay.sleep(125);
 			copyAndZeroOut(nextGen, currentGen);
 		}
 		return currentGen;
 	}
-	
+
 	// Contains the logic for the startingWorld.
 	// Creates cells that are alive or dead in Gen 0.
 	// Allocated and return the starting matrix of size 'dimen'.
-	
-	
+
 	private int[][] createRandomStart(Integer dimen) {
-		//create Random Start
-		
+		// create Random Start
+
 		currentGen = new int[dimen][dimen];
 		for (int i = 0; i < dimen; i++) {
 			for (int j = 0; j < dimen; j++) {
-				if(Math.random() * 100 > 50) {
+				if (Math.random() * 100 > 50) {
 					currentGen[i][j] = 1;
 				} else {
 					currentGen[i][j] = 0;
@@ -68,26 +66,36 @@ public class cgol {
 		}
 		return currentGen;
 	}
-	
+
 	// Copy the values of "Next" matrix to "current" matrix as Gen.
 	// And then Zero Out the contents of "Next" matrix.
-	
-	public void copyAndZeroOut(int [][] next, int [][] current) {
+
+	public void copyAndZeroOut(int[][] next, int[][] current) {
+		for (int i = 0; i < next.length; i++) {
+			for (int j = 0; j < next.length; j++) {
+				next[i][j] = isAlive(i, j, current);
+			}
+		}
+		for (int i = 0; i < next.length; i++) {
+			for (int j = 0; j < next.length; j++) {
+				current[i][j] = next[i][j];
+				next[i][j] = 0;
+			}
+		}
 	}
-	
+
 	// calculate if any individual cell should be alive in the next gen
 	// Based on the my game logic :
 	/*
-	   Any live cell with fewer than two live neighbors dies, as if by needs caused by underpopulation.
-	   Any live cell with more than three live neighbors dies, as if by overcrowding.
-       Any live cell with two or three live neighbors lives, unchanged, to the next generation.
-       Any dead cell with exactly three live neighbors cells will come to life.
+	 * Any live cell with fewer than two live neighbors dies, as if by needs caused
+	 * by underpopulation. Any live cell with more than three live neighbors dies,
+	 * as if by overcrowding. Any live cell with two or three live neighbors lives,
+	 * unchanged, to the next generation. Any dead cell with exactly three live
+	 * neighbors cells will come to life.
 	 */
-	
+
 	private int isAlive(int row, int col, int[][] world) {
 		return 0;
 	}
-	
-	
 
 }
